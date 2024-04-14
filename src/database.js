714b0680bc82e80eaae0dd35ef71ed42dcccab17
Ale <AROrtega@hotmail.com>
2024-04-14 15:16:00 -0300

    Base de datos nueva y funcional. Register, login, logout, add to cart, delete to cart and create product funcionan y se renderizan

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://swtocaimaza:coderhouse@cluster0.pmzgicx.mongodb.net/E-commerce?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => console.log("Conexión exitosa"))
-    .catch(() => console.log("Vamos a morir, tenemos un error"))
+mongoose.connect("mongodb+srv://aleortega:coderhouse@cluster0.oprbhbr.mongodb.net/ecommerce50015?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log("Success: Se conecto correctamente con MongoDB"))
+    .catch(() => console.log("Failed: No se pudo conectar con MongoDB"))
     
